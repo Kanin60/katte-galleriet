94ab34be5dad6621bdeebaba91cea725f2a8968d
Amina <113165708+asmina18@users.noreply.github.com>
2023-09-27 15:11:43 +0200

    Gallery færdig

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,36 @@ function App() {
   const [headerImg, setHeaderImg] = useState()
 
   const client = contentful.createClient({
-    space: `${import.meta.env.VITE_PUBLIC_SPACE_ID}`,//Husk at udfyld .env-filen
+    space: `${import.meta.env.VITE_PUBLIC_SPACE_ID}`,
     environment: "master",
     accessToken: `${import.meta.env.VITE_PUBLIC_ACCESS_TOKEN}`,
   })
 
   useEffect(() => {
-    client.getEntries().then((entry) => setHeaderImg(entry)).catch(console.error())
+    client.getEntries()
+      .then((entry) => setHeaderImg(entry))
+      .catch(console.error())
+
+
   }, [])
   console.log(headerImg);
 
   return (
     <>
+      <h1>Gallery</h1>
+      {headerImg?.items.map((item, index) => {
+        return (
+          
+          <article key={index}>
+         
+            {documentToReactComponents(item.fields.description)}
+
+          {item.fields.galleryImage && <img src={`https:${item.fields.galleryImage.fields.file.url}`} />}
+        </article>
+
+        )
+      })}
+
 
     </>
   )
